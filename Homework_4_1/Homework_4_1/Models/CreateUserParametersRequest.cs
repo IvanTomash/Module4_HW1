767f8ec5eeeee96ba4c3c2e08e4e91f77f7d0213
IvanTomash <tomashuckvanya@gmail.com>
2023-09-13 14:09:50 +0300

    First commit HW_4_1

---
+++ Homework_4_1/Homework_4_1/Models/CreateUserParametersRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_1.Models
+{
+    internal sealed class CreateUserParametersRequest
+    {
+        public string Name { get; set; }
+
+        public string Job { get; set; }
+    }
+}
---
+++ Homework_4_1/Homework_4_1/Models/ReqresUserRequest.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_1.Models
+{
+    internal sealed class ReqresUserRequest
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        public string Avatar { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} | Email: {Email} | FirstName: {FirstName} | LastName: {LastName} | Avatar: {Avatar}";
+        }
+    }
+}
